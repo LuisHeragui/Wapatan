e7f14d554c40faa92d875c9062f212be97e9fe41
Luis Alberto Hernández Aguilar <luis@m22.mx>
2024-11-16 00:57:59 -0600

    Adición de los precios

--- database/migrations/2024_11_13_064918_create_products_table.php
+++ database/migrations/2024_11_13_064918_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->string('svg');
+            $table->decimal('price', 9, 2);
             $table->timestamps();
         });
     }
--- resources/js/Pages/Wapatan.vue
+++ resources/js/Pages/Wapatan.vue
@@ -4,7 +4,9 @@ import PreviousMap from 'postcss/lib/previous-map';
 import { ref } from 'vue'
 
 const productSelected = ref(false);
+const showModal = ref(false);
 const currentWaps = ref([]);
+const total = ref(0);
 
 const props = defineProps({
     products: {
@@ -21,18 +23,40 @@ function addProduct(product) {
     productSelected.value = true;
     currentWaps.value.push({
         product: product,
-        ingredients: props.ingredients.map(i => ({...i, portions: 0}))
+        ingredients: props.ingredients.map(i => ({ ...i, portions: 0 })),
+        total: product.price
     });
+}
+
+function checkOrder() {
+    showModal.value = true;
+    total.value = currentWaps.value.reduce((accumulator, current) => accumulator + parseFloat(current.total), 0).toFixed(2);
 }
 
 function makeOrder() {
     console.log('order', currentWaps.value);
 }
+
+function removeWap(index) {
+    currentWaps.value.splice(index, 1);
+    if (!currentWaps.value.length) {
+        productSelected.value = false;
+    }
+}
+
+function updateTotal(index) {
+    let total = 0;
+    currentWaps.value[index].ingredients.forEach(ing => {
+        total += (ing.price * ing.portions)
+    });
+    total += parseFloat(currentWaps.value[index].product.price);
+    currentWaps.value[index].total = total.toFixed(2);
+}
 </script>
 
 <template>
     <Head title="Wapatan" />
-    <div class="bg-black bg-[url('/storage/bg.jpg')]">
+    <div class="relative bg-black bg-[url('/storage/bg.jpg')]">
         <div
             class="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
@@ -47,7 +71,7 @@ function makeOrder() {
                     </div>
                 </header>
 
-                <main class="mt-6">
+                <main>
                     <div v-if="!productSelected" class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                         <div class="flex items-start gap-4 justify-center rounded-lg p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring ring-white ring-inset ring-offset-2 bg-[#E6D0BE] hover:bg-[#DDBFA7] hover:cursor-pointer"
                             v-for="product in products" :key="product.id" @click="addProduct(product)">
@@ -58,7 +82,7 @@ function makeOrder() {
 
                             <div>
                                 <h2 class="text-xl font-semibold text-black">
-                                    {{ product.name }}
+                                    {{ product.name }} (${{ product.price }})
                                 </h2>
 
                                 <p class="mt-4 text-sm/relaxed text-black">
@@ -71,7 +95,13 @@ function makeOrder() {
                     <div v-else class="text-center">
                         <div class="container mx-auto flex gap-4 overflow-x-auto p-4">
                             <div v-for="(wapatan, key) in currentWaps" :key="key"
-                                class="flex flex-col gap-4 rounded-lg p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] bg-[#E6D0BE] ring ring-white ring-offset-2 shrink-0">
+                                class="relative flex flex-col gap-4 rounded-lg p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] bg-[#E6D0BE] ring ring-white ring-offset-2 shrink-0">
+                                <button type="button" class="absolute right-2 top-2 w-4" @click="removeWap(key)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                        <path
+                                            d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                                    </svg>
+                                </button>
                                 <h2 class="text-xl font-semibold text-black">
                                     Selecciona los ingredientes para tu {{ wapatan.product.name }}
                                 </h2>
@@ -79,12 +109,13 @@ function makeOrder() {
                                 <div class="flex flex-col gap-2 rounded-lg px-4 py-2 bg-[#FBFFAF] text-center"
                                     v-for="(ing, ingKey) in ingredients" :key="ing.id">
                                     <h3 class="text-xl font-semibold text-black">
-                                        {{ ing.name }}
+                                        {{ ing.name }} (${{ ing.price }})
                                     </h3>
                                     <div>
                                         <label :for="`${ing.name}-${key}`" class="text-black">Porciones: </label>
-                                        <input type="number" :name="`${ing.name}-${key}`" min="0" v-model="wapatan.ingredients[ingKey].portions"
-                                            class="text-black">
+                                        <input type="number" :name="`${ing.name}-${key}`" min="0"
+                                            v-model="wapatan.ingredients[ingKey].portions" class="text-black"
+                                            @change="updateTotal(key)">
                                     </div>
                                 </div>
                             </div>
@@ -102,7 +133,7 @@ function makeOrder() {
 
                                     <div>
                                         <h2 class="text-xl font-semibold text-black">
-                                            {{ product.name }}
+                                            {{ product.name }} (${{ product.price }})
                                         </h2>
 
                                         <p class="mt-4 text-sm/relaxed text-black">
@@ -114,13 +145,54 @@ function makeOrder() {
                         </div>
                         <button type="button"
                             class="mt-6 px-6 py-4 rounded-lg bg-[#D0E37F] text-black font-bold text-xl hover:bg-[#9EB828] hover:text-white"
-                            @click="makeOrder()">Realizar pedido</button>
+                            @click="checkOrder()">Realizar pedido</button>
                     </div>
                 </main>
 
                 <footer class="py-16 text-center text-sm text-white font-bold">
                     Wapatan© 2024
                 </footer>
+            </div>
+        </div>
+
+        <div v-if="showModal" class="absolute flex w-full h-full top-0 right-0 bg-black/50">
+            <div class="m-auto bg-emerald-50 p-5 rounded-lg w-1/3 max-h-[500px]">
+                <h2 class="font-bold text-xl mb-5 text-center">Confirma tu pedido</h2>
+
+                <div class="bg-gray-100 p-2 rounded-lg h-auto max-h-[330px] overflow-y-auto">
+                    <div class="grid grid-cols-2 gap-4">
+                        <h3 class="text-center text-lg font-bold">Wapatan</h3>
+                        <h3 class="text-center text-lg font-bold">Precio</h3>
+                    </div>
+
+                    <div v-for="(wapatan, key) in currentWaps" :key="key" class="my-2">
+                        <div class="grid grid-cols-2 gap-x-4">
+                            <h4 class="font-bold text-lg">{{ wapatan.product.name }}</h4>
+                            <h4 class="font-bold text-lg text-center">${{ wapatan.total }}</h4>
+                            <template v-for="ing in wapatan.ingredients" :key="ing.id">
+                                <template v-if="ing.portions != 0">
+                                    <p class="ms-3">${{ ing.name }} (x{{ ing.portions }})</p>
+                                    <p class="text-center">${{ (ing.price * ing.portions).toFixed(2) }}</p>
+                                </template>
+                            </template>
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-2 gap-4">
+                        <h3 class="text-center text-lg font-bold">Total</h3>
+                        <h3 class="text-center text-lg font-bold">${{ total }}</h3>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-2 gap-x-4">
+                    <button type="button"
+                        class="mt-6 px-6 py-4 rounded-lg bg-[#F2A6B5] text-black font-bold text-xl hover:bg-[#EA7189] hover:text-white"
+                        @click="showModal = false">Cancelar</button>
+                    <button type="button"
+                        class="mt-6 px-6 py-4 rounded-lg bg-[#D0E37F] text-black font-bold text-xl hover:bg-[#9EB828] hover:text-white"
+                        @click="makeOrder()">Confirmar</button>
+                </div>
+
             </div>
         </div>
     </div>
