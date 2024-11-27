43c52bbb2f7bad55e9510a911dafd0f37034aec9
Luis Alberto Hernández Aguilar <luis@m22.mx>
2024-11-26 21:54:17 -0600

    Mostrando ticket final

--- resources/js/Pages/Wapatan.vue
+++ resources/js/Pages/Wapatan.vue
@@ -5,6 +5,7 @@ import { ref } from 'vue'
 
 const productSelected = ref(false);
 const showModal = ref(false);
+const showTicket = ref(false);
 const currentWaps = ref([]);
 const total = ref(0);
 
@@ -34,6 +35,8 @@ function checkOrder() {
 }
 
 function makeOrder() {
+    showModal.value = false;
+    showTicket.value = true;
     console.log('order', currentWaps.value);
 }
 
@@ -42,6 +45,12 @@ function removeWap(index) {
     if (!currentWaps.value.length) {
         productSelected.value = false;
     }
+}
+
+function restart() {
+    productSelected.value = false;
+    showTicket.value = false;
+    currentWaps.value = [];
 }
 
 function updateTotal(index) {
@@ -62,12 +71,7 @@ function updateTotal(index) {
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                 <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                     <div class="flex lg:col-start-2 lg:justify-center">
-                        <svg class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]" viewBox="0 0 62 65" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M61.8548 14.6253C61.8778 14.7102 61.8895 14.7978 61.8897 14.8858V28.5615C61.8898 28.737 61.8434 28.9095 61.7554 29.0614C61.6675 29.2132 61.5409 29.3392 61.3887 29.4265L49.9104 36.0351V49.1337C49.9104 49.4902 49.7209 49.8192 49.4118 49.9987L25.4519 63.7916C25.3971 63.8227 25.3372 63.8427 25.2774 63.8639C25.255 63.8714 25.2338 63.8851 25.2101 63.8913C25.0426 63.9354 24.8666 63.9354 24.6991 63.8913C24.6716 63.8838 24.6467 63.8689 24.6205 63.8589C24.5657 63.8389 24.5084 63.8215 24.456 63.7916L0.501061 49.9987C0.348882 49.9113 0.222437 49.7853 0.134469 49.6334C0.0465019 49.4816 0.000120578 49.3092 0 49.1337L0 8.10652C0 8.01678 0.0124642 7.92953 0.0348998 7.84477C0.0423783 7.8161 0.0598282 7.78993 0.0697995 7.76126C0.0884958 7.70891 0.105946 7.65531 0.133367 7.6067C0.152063 7.5743 0.179485 7.54812 0.20192 7.51821C0.230588 7.47832 0.256763 7.43719 0.290416 7.40229C0.319084 7.37362 0.356476 7.35243 0.388883 7.32751C0.425029 7.29759 0.457436 7.26518 0.498568 7.2415L12.4779 0.345059C12.6296 0.257786 12.8015 0.211853 12.9765 0.211853C13.1515 0.211853 13.3234 0.257786 13.475 0.345059L25.4531 7.2415H25.4556C25.4955 7.26643 25.5292 7.29759 25.5653 7.32626C25.5977 7.35119 25.6339 7.37362 25.6625 7.40104C25.6974 7.43719 25.7224 7.47832 25.7523 7.51821C25.7735 7.54812 25.8021 7.5743 25.8196 7.6067C25.8483 7.65656 25.8645 7.70891 25.8844 7.76126C25.8944 7.78993 25.9118 7.8161 25.9193 7.84602C25.9423 7.93096 25.954 8.01853 25.9542 8.10652V33.7317L35.9355 27.9844V14.8846C35.9355 14.7973 35.948 14.7088 35.9704 14.6253C35.9792 14.5954 35.9954 14.5692 36.0053 14.5405C36.0253 14.4882 36.0427 14.4346 36.0702 14.386C36.0888 14.3536 36.1163 14.3274 36.1375 14.2975C36.1674 14.2576 36.1923 14.2165 36.2272 14.1816C36.2559 14.1529 36.292 14.1317 36.3244 14.1068C36.3618 14.0769 36.3942 14.0445 36.4341 14.0208L48.4147 7.12434C48.5663 7.03694 48.7383 6.99094 48.9133 6.99094C49.0883 6.99094 49.2602 7.03694 49.4118 7.12434L61.3899 14.0208C61.4323 14.0457 61.4647 14.0769 61.5021 14.1055C61.5333 14.1305 61.5694 14.1529 61.5981 14.1803C61.633 14.2165 61.6579 14.2576 61.6878 14.2975C61.7103 14.3274 61.7377 14.3536 61.7551 14.386C61.7838 14.4346 61.8 14.4882 61.8199 14.5405C61.8312 14.5692 61.8474 14.5954 61.8548 14.6253ZM59.893 27.9844V16.6121L55.7013 19.0252L49.9104 22.3593V33.7317L59.8942 27.9844H59.893ZM47.9149 48.5566V37.1768L42.2187 40.4299L25.953 49.7133V61.2003L47.9149 48.5566ZM1.99677 9.83281V48.5566L23.9562 61.199V49.7145L12.4841 43.2219L12.4804 43.2194L12.4754 43.2169C12.4368 43.1945 12.4044 43.1621 12.3682 43.1347C12.3371 43.1097 12.3009 43.0898 12.2735 43.0624L12.271 43.0586C12.2386 43.0275 12.2162 42.9888 12.1887 42.9539C12.1638 42.9203 12.1339 42.8916 12.114 42.8567L12.1127 42.853C12.0903 42.8156 12.0766 42.7707 12.0604 42.7283C12.0442 42.6909 12.023 42.656 12.013 42.6161C12.0005 42.5688 11.998 42.5177 11.9931 42.4691C11.9881 42.4317 11.9781 42.3943 11.9781 42.3569V15.5801L6.18848 12.2446L1.99677 9.83281ZM12.9777 2.36177L2.99764 8.10652L12.9752 13.8513L22.9541 8.10527L12.9752 2.36177H12.9777ZM18.1678 38.2138L23.9574 34.8809V9.83281L19.7657 12.2459L13.9749 15.5801V40.6281L18.1678 38.2138ZM48.9133 9.14105L38.9344 14.8858L48.9133 20.6305L58.8909 14.8846L48.9133 9.14105ZM47.9149 22.3593L42.124 19.0252L37.9323 16.6121V27.9844L43.7219 31.3174L47.9149 33.7317V22.3593ZM24.9533 47.987L39.59 39.631L46.9065 35.4555L36.9352 29.7145L25.4544 36.3242L14.9907 42.3482L24.9533 47.987Z"
-                                fill="currentColor" />
-                        </svg>
+                        <img :src="`/storage/logo.png`" alt="Logo" width="100">
                     </div>
                 </header>
 
@@ -92,60 +96,102 @@ function updateTotal(index) {
                         </div>
                     </div>
 
-                    <div v-else class="text-center">
-                        <div class="container mx-auto flex gap-4 overflow-x-auto p-4">
-                            <div v-for="(wapatan, key) in currentWaps" :key="key"
-                                class="relative flex flex-col gap-4 rounded-lg p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] bg-[#E6D0BE] ring ring-white ring-offset-2 shrink-0">
-                                <button type="button" class="absolute right-2 top-2 w-4" @click="removeWap(key)">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                                        <path
-                                            d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
-                                    </svg>
-                                </button>
-                                <h2 class="text-xl font-semibold text-black">
-                                    Selecciona los ingredientes para tu {{ wapatan.product.name }}
-                                </h2>
+                    <div v-else>
+                        <div v-if="showTicket">
+                            <h2 class="text-3xl font-semibold text-white text-center mb-2">
+                                Resumen de tu pedido
+                            </h2>
+                            <h3 class="text-xl font-semibold text-white text-center mb-2">
+                                Puedes pasar a la caja a pagar
+                            </h3>
 
-                                <div class="flex flex-col gap-2 rounded-lg px-4 py-2 bg-[#FBFFAF] text-center"
-                                    v-for="(ing, ingKey) in ingredients" :key="ing.id">
-                                    <h3 class="text-xl font-semibold text-black">
-                                        {{ ing.name }} (${{ ing.price }})
-                                    </h3>
-                                    <div>
-                                        <label :for="`${ing.name}-${key}`" class="text-black">Porciones: </label>
-                                        <input type="number" :name="`${ing.name}-${key}`" min="0"
-                                            v-model="wapatan.ingredients[ingKey].portions" class="text-black"
-                                            @change="updateTotal(key)">
+                            <div class="bg-gray-100 p-2 rounded-lg h-auto max-h-[330px] overflow-y-auto">
+                                <div class="grid grid-cols-2 gap-4">
+                                    <h3 class="text-center text-lg font-bold">Wapatan</h3>
+                                    <h3 class="text-center text-lg font-bold">Precio</h3>
+                                </div>
+    
+                                <div v-for="(wapatan, key) in currentWaps" :key="key" class="my-2">
+                                    <div class="grid grid-cols-2 gap-x-4">
+                                        <h4 class="font-bold text-lg">{{ wapatan.product.name }}</h4>
+                                        <h4 class="font-bold text-lg text-center">${{ wapatan.total }}</h4>
+                                        <template v-for="ing in wapatan.ingredients" :key="ing.id">
+                                            <template v-if="ing.portions != 0">
+                                                <p class="ms-3">${{ ing.name }} (x{{ ing.portions }})</p>
+                                                <p class="text-center">${{ (ing.price * ing.portions).toFixed(2) }}</p>
+                                            </template>
+                                        </template>
                                     </div>
+                                </div>
+    
+                                <div class="grid grid-cols-2 gap-4">
+                                    <h3 class="text-center text-lg font-bold">Total</h3>
+                                    <h3 class="text-center text-lg font-bold">${{ total }}</h3>
                                 </div>
                             </div>
-                            <div class="flex flex-col gap-6 rounded-lg p-6 text-center shrink-0">
-                                <h2 class="text-xl font-semibold text-white">
-                                    Agregar un Wapatan
-                                </h2>
 
-                                <div class="flex items-start gap-4 justify-center rounded-lg p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring ring-white ring-inset ring-offset-2 bg-[#E6D0BE] hover:bg-[#DDBFA7] hover:cursor-pointer"
-                                    v-for="product in products" :key="product.id" @click="addProduct(product)">
-                                    <div
-                                        class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FBFFAF] sm:size-16">
-                                        <img :src="`/storage/${product.svg}.svg`" :alt="product.name">
-                                    </div>
-
-                                    <div>
-                                        <h2 class="text-xl font-semibold text-black">
-                                            {{ product.name }} (${{ product.price }})
-                                        </h2>
-
-                                        <p class="mt-4 text-sm/relaxed text-black">
-                                            {{ product.description }}
-                                        </p>
-                                    </div>
-                                </div>
+                            <div class="text-center">
+                                <button type="button"
+                                    class="mt-6 px-6 py-4 text-center rounded-lg bg-[#D0E37F] text-black font-bold text-xl hover:bg-[#9EB828] hover:text-white"
+                                    @click="restart()">Volver al inicio</button>
                             </div>
                         </div>
-                        <button type="button"
-                            class="mt-6 px-6 py-4 rounded-lg bg-[#D0E37F] text-black font-bold text-xl hover:bg-[#9EB828] hover:text-white"
-                            @click="checkOrder()">Realizar pedido</button>
+
+                        <div v-else class="text-center">
+                            <div class="container mx-auto flex gap-4 overflow-x-auto p-4">
+                                <div v-for="(wapatan, key) in currentWaps" :key="key"
+                                    class="relative flex flex-col gap-4 rounded-lg p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] bg-[#E6D0BE] ring ring-white ring-offset-2 shrink-0">
+                                    <button type="button" class="absolute right-2 top-2 w-4" @click="removeWap(key)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                            <path
+                                                d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                                        </svg>
+                                    </button>
+                                    <h2 class="text-xl font-semibold text-black">
+                                        Selecciona los ingredientes para tu {{ wapatan.product.name }}
+                                    </h2>
+
+                                    <div class="flex flex-col gap-2 rounded-lg px-4 py-2 bg-[#FBFFAF] text-center"
+                                        v-for="(ing, ingKey) in ingredients" :key="ing.id">
+                                        <h3 class="text-xl font-semibold text-black">
+                                            {{ ing.name }} (${{ ing.price }})
+                                        </h3>
+                                        <div>
+                                            <label :for="`${ing.name}-${key}`" class="text-black">Porciones: </label>
+                                            <input type="number" :name="`${ing.name}-${key}`" min="0"
+                                                v-model="wapatan.ingredients[ingKey].portions" class="text-black"
+                                                @change="updateTotal(key)">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-col gap-6 rounded-lg p-6 text-center shrink-0">
+                                    <h2 class="text-xl font-semibold text-white">
+                                        Agregar un Wapatan
+                                    </h2>
+
+                                    <div class="flex items-start gap-4 justify-center rounded-lg p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring ring-white ring-inset ring-offset-2 bg-[#E6D0BE] hover:bg-[#DDBFA7] hover:cursor-pointer"
+                                        v-for="product in products" :key="product.id" @click="addProduct(product)">
+                                        <div
+                                            class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FBFFAF] sm:size-16">
+                                            <img :src="`/storage/${product.svg}.svg`" :alt="product.name">
+                                        </div>
+
+                                        <div>
+                                            <h2 class="text-xl font-semibold text-black">
+                                                {{ product.name }} (${{ product.price }})
+                                            </h2>
+
+                                            <p class="mt-4 text-sm/relaxed text-black">
+                                                {{ product.description }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="button"
+                                class="mt-6 px-6 py-4 rounded-lg bg-[#D0E37F] text-black font-bold text-xl hover:bg-[#9EB828] hover:text-white"
+                                @click="checkOrder()">Realizar pedido</button>
+                        </div>
                     </div>
                 </main>
 
